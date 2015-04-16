1c2df737ca7b893f3acab927bbfc5387eb90ea8e
Gareth Evans <gareth.evans@switchconcepts.com>
2015-04-16 09:02:22 +0100

    Unexported romanNumeralConvertion and added comments around other exported vars

--- converter.go
+++ converter.go
@@ -1,29 +1,31 @@
 package converter
 
+// RomanNumeralConverter is the public strcut for the roman numeral convertion
 type RomanNumeralConverter struct {
 }
 
-type RomanNumeralConvertion struct {
+type romanNumeralConvertion struct {
     number int
     numeral string
 }
 
-var mapping = []RomanNumeralConvertion{
-    RomanNumeralConvertion{1000, "M"},
-    RomanNumeralConvertion{900, "CM"},
-    RomanNumeralConvertion{500, "D"},
-    RomanNumeralConvertion{400, "CD"},
-    RomanNumeralConvertion{100, "C"},
-    RomanNumeralConvertion{90, "XC"},
-    RomanNumeralConvertion{50, "L"},
-    RomanNumeralConvertion{40, "XL"},
-    RomanNumeralConvertion{10, "X"},
-    RomanNumeralConvertion{9, "IX"},
-    RomanNumeralConvertion{5, "V"},
-    RomanNumeralConvertion{4, "IV"},
-    RomanNumeralConvertion{1, "I"},
+var mapping = []romanNumeralConvertion{
+    romanNumeralConvertion{1000, "M"},
+    romanNumeralConvertion{900, "CM"},
+    romanNumeralConvertion{500, "D"},
+    romanNumeralConvertion{400, "CD"},
+    romanNumeralConvertion{100, "C"},
+    romanNumeralConvertion{90, "XC"},
+    romanNumeralConvertion{50, "L"},
+    romanNumeralConvertion{40, "XL"},
+    romanNumeralConvertion{10, "X"},
+    romanNumeralConvertion{9, "IX"},
+    romanNumeralConvertion{5, "V"},
+    romanNumeralConvertion{4, "IV"},
+    romanNumeralConvertion{1, "I"},
 }
 
+// Convert will convert any number to it's roman numeral equivalent
 func (r *RomanNumeralConverter) Convert(number int) string {
     var numeral string;
 
